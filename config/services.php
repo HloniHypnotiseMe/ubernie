97798d49fbfb8e3a616edb2d906c0b57fa897f2e
HloniHypnotiseMe <Hlonihypnotiseme@gmail.com>
2026-08-24 13:32:26 +0200

    fix: configure Ubernie RemotePay merchant identity

--- config/services.php
+++ config/services.php
@@ -4,5 +4,7 @@ return [
     'remotepay' => [
         'base_url' => env('REMOTEPAY_API_BASE_URL'),
         'timeout' => env('REMOTEPAY_API_TIMEOUT', 15),
+        'merchant_id' => env('REMOTEPAY_MERCHANT_ID', 'ubernie'),
+        'brand_id' => env('REMOTEPAY_BRAND_ID', 'ubernie'),
     ],
 ];
